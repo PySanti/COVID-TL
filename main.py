0c80a7f8620320971bbf56e66d1e8e423cea951f
Santiago <santiagodeandrade4@gmail.com>
2025-09-15 21:57:27 -0400

    Avances en el proceso de entrenamiento

--- main.py
+++ main.py
@@ -1,14 +1,80 @@
 import kagglehub
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
 from utils.NormDataset import NormDataset
 from utils.load_dataset import load_dataset
+from utils.load_model import load_model
 from utils.normalization_metrics_calc import normalization_metrics_calc
+from utils.ImagesDataset import ImagesDataset
+from torchvision import transforms
+from utils.MACROS import BATCH_SIZE, EPOCHS, MEANS, STDS
+import torchvision
 
 if __name__ == "__main__":
 
     data_path = kagglehub.dataset_download("andyczhao/covidx-cxr2")
-    train_X, train_Y = load_dataset('train', data_path)
-    val_X, val_Y = load_dataset('val', data_path)
-    test_X, test_Y = load_dataset('test', data_path)
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    trainset = NormDataset(train_X)
-    print(normalization_metrics_calc(trainset))
+    train_transformer  = transforms.Compose([
+
+        # basic resize
+        
+        transforms.Resize((256, 256)), # redimensionar 
+        transforms.CenterCrop((224, 224)), # recordar desde el centro para consistencia
+        
+        # data aumentation
+
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1),
+        transforms.RandomRotation(15),
+
+
+        transforms.ToTensor(),
+        transforms.Normalize(MEANS, STDS)
+    ])
+
+    val_transformer = transforms.Compose([
+        transforms.Resize((256, 256)), 
+        transforms.CenterCrop((224, 224)), 
+
+        transforms.ToTensor(),
+        transforms.Normalize(MEANS, STDS)
+    ])
+
+    trainset = ImagesDataset(*load_dataset('train', data_path), train_transformer)
+    valset = ImagesDataset(*load_dataset('val', data_path), val_transformer)
+    testset = ImagesDataset(*load_dataset('test', data_path), val_transformer)
+
+
+    trainloader = DataLoader(trainset, BATCH_SIZE, shuffle=True, num_workers=8, persistent_workers=True, pin_memory=True)
+    valloader = DataLoader(valset, BATCH_SIZE, shuffle=True, num_workers=8, persistent_workers=True, pin_memory=True)
+
+    model = load_model(2).to(DEVICE)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    for i in range(EPOCHS):
+
+        batches_train_loss = []
+        batches_val_loss = []
+
+        for a, (X_batch, Y_batch) in enumerate(trainloader):
+            print(f"batch : {a}/{len(trainloader)}", end="\r")
+            X_batch, Y_batch = X_batch.to(DEVICE), Y_batch.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            output = model(X_batch)
+            loss = criterion(output, Y_batch)
+            loss.backward()
+            optimizer.step()
+
+
+            batches_train_loss.append(loss.item())
+
+        print("\n\n")
+
+        print(f"Epoch : {i}/{len(trainloader)}, Loss : {np.mean(batches_train_loss)}")
+
---
+++ utils/load_model.py
@@ -0,0 +1,13 @@
+import torchvision
+from torchvision.models import  resnet18
+import torch
+
+def load_model(num_class):
+    model = resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_features = model.fc.in_features
+    model.fc = torch.nn.Linear(num_features, 2)  # 2 clases: COVID / No-COVID
+    return model
